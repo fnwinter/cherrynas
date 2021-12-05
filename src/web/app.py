e2acd21bc01351218ec4737004242e79701b805a
JungJik Lee <fnwinter@gmail.com>
2021-12-06 02:40:04 +0900

    login db

--- src/web/app.py
+++ src/web/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
 from flask import render_template, redirect
 
+from sqlalchemy import create_engine
+
 from account.login_form import LoginForm
+from account.signup_form import SignUpForm
+from database.login_db import Account
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '12345643214321432143214321'
+
+engine = create_engine('sqlite:///account.db', echo=True)
+Account.__table__.create(bind=engine, checkfirst=True)
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+session = Session()
 
 @app.route("/")
 def main():
@@ -14,6 +25,11 @@ def main():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        q = session.query(Account).filter_by(email="%s" % form.email, password="%s"%form.password)
+        if q.first():
+            print("found")
+        else:
+            print("not found")
         return redirect('/')
     return render_template('/account/login.html', form=form)
 
@@ -21,6 +37,13 @@ def login():
 def logout():
     return render_template('/account/logout.html')
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template('/account/signup.html')
+    form = SignUpForm()
+    if form.validate_on_submit():
+        print(form.email, form.password)
+        account = Account(email="%s" % form.email, password="%s"%form.password)
+        session.add(account)
+        session.commit()
+        return redirect('/')
+    return render_template('/account/signup.html', form=form)
---
+++ src/web/database/login_db.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+
+Base = declarative_base()
+
+class Account(Base):
+    __tablename__ = 'account'
+    email = Column(String, primary_key=True, nullable=False)
+    password = Column(String, nullable=False)
