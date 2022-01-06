1ce876ebfc9724794f10f8bf31df4faef75c2221
JungJik Lee <fnwinter@gmail.com>
2022-01-07 01:30:59 +0900

    show file list

--- src/web/explorer/explorer_view.py
+++ src/web/explorer/explorer_view.py
@@ -9,5 +9,20 @@ class ExplorerView(FlaskView):
     def index(self):
         session['explorer_path'] = '/'
 
-        file_list = os.listdir("./")
+        _path = "/mnt/c/Windows"
+        
+        folder = [f for f in os.listdir(_path) if os.path.isdir(os.path.join(_path, f))]
+        files = [f for f in os.listdir(_path) if os.path.isfile(os.path.join(_path, f))]
+
+        file_list = []
+        file_list.append(["..","folder"])
+        for f in folder:
+            file_list.append([f,"folder"])
+        for f in files:
+            extension = f[-3:]
+            icon = "file"
+            if extension == 'txt' or extension == 'log':
+                icon = "file-text"
+            file_list.append([f,icon])
+        
         return render_template('/explorer/explorer.html', files = file_list)
