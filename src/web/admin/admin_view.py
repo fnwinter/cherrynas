d90450d7603eaa7bfc35da4276085d07707d516a
JungJik Lee <fnwinter@gmail.com>
2022-02-19 01:01:32 +0900

    admin page

--- src/web/admin/admin_view.py
+++ src/web/admin/admin_view.py
@@ -2,6 +2,15 @@ from flask_classful import FlaskView, route
 from flask import render_template, session, redirect
 
 from web.admin.admin_form import AdminForm
+from web.database.account_db import Account
+from web import DB
+
+class member:
+    id = 0
+    email = None
+    nick_name = None
+    joined = False
+    permission = 'all'
 
 class AdminView(FlaskView):
     """
@@ -24,15 +33,55 @@ class AdminView(FlaskView):
         elif session.get('email'):
             who = f"{session.get('email')}"
 
-        _form = AdminForm()
+        form_ = AdminForm()
 
-        if _form.validate_on_submit():
-            data = _form['data'].data
-            print(data)
+        member_list_ = self.load_member_lists()
 
-        return render_template('/admin/admin.html', email=who, form=_form)
+        return render_template('/admin/admin.html', email=who, form=form_, member_list=member_list_)
 
     def post(self):
-        _form = AdminForm()
+        member_list_ = self.load_member_lists()
 
-        return render_template('/admin/admin.html', form=_form)
+        who = None
+        if session.get('nick_name'):
+            who = f"{session.get('nick_name')}"
+        elif session.get('email'):
+            who = f"{session.get('email')}"
+
+        form_ = AdminForm()
+        if form_.validate_on_submit():
+            data = form_['data'].data
+            print("data > " + data)
+
+        return render_template('/admin/admin.html', email=who, form=form_, member_list=member_list_)
+
+    def load_member_lists(self):
+        member_list_ = []
+        result = DB.session.query(Account).all()
+        index = 0
+        for acount in result:
+            member_ = member
+            member_.id = index
+            member_.email = acount.email
+            member_.joined = acount.joined
+            member_.nick_name = acount.nick_name
+            member_.permission = acount.permission
+            member_list_.append(member_)
+            index += 1
+
+            member_ = member
+            member_.id = index
+            member_.email = acount.email
+            member_.joined = acount.joined
+            member_.nick_name = acount.nick_name
+            member_.permission = acount.permission
+            member_list_.append(member_)
+            index += 1
+
+        return member_list_
+
+    def delete_member(self, list_to_delete):
+        pass
+
+    def apply_change(self, member_list):
+        pass
--- src/web/database/account_db.py
+++ src/web/database/account_db.py
@@ -6,4 +6,5 @@ class Account(DB.Model):
     nick_name = DB.Column(DB.String(30))
     password = DB.Column(DB.String, nullable=False)
     reset_pin = DB.Column(DB.String(6))
-    allowed_by_admin = DB.Column(DB.Boolean, default=False)
+    joined = DB.Column(DB.Boolean, default=False)
+    permission = DB.Column(DB.String, default='all')
