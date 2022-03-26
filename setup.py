2bc71babddd95050ccec24241f60ed1b2a585424
JungJik Lee <fnwinter@gmail.com>
2022-03-27 00:12:23 +0900

    update pathlib

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
-import pathlib
 import pkg_resources
 
 from setuptools import setup, find_packages
-
-with pathlib.Path('./requirements.txt').open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
-    ]
 
 setup(name='cherrynas',
       version='0.0.1',
@@ -20,4 +12,4 @@ setup(name='cherrynas',
       package_dir={'cherrynas': 'cherrynas'},
       long_description='',
       zip_safe=False,
-      setup_requires=install_requires)
+      setup_requires='')
