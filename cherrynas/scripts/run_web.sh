6d87a5acb876882c589052681247fb5f27faa8b0
JungJik Lee <fnwinter@gmail.com>
2022-03-27 12:23:04 +0900

    visible

--- cherrynas/scripts/run_web.sh
+++ cherrynas/scripts/run_web.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )"
 pushd $SCRIPT_DIR/../.. > /dev/null
   export FLASK_APP=cherrynas.web
   export FLASK_DEBUG=1
-  flask run
+  flask run --host=0.0.0.0
 popd > /dev/null
