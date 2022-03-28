c7ae9b7df7d1b471f1132282b39e34207e4b7488
JungJik Lee <fnwinter@gmail.com>
2022-03-29 00:17:31 +0900

    nit

--- freeze.py
+++ freeze.py
@@ -1,19 +1,41 @@
-import os
+#!/usr/bin/python3
+#
+# Copyright 2022 fnwinter@gmail.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import subprocess
 
 if __name__ == '__main__':
-  requirements = None
-  process_ = subprocess.run(['pip', 'freeze'], capture_output=True, text=True)
-  requirements = process_.stdout.splitlines()
-  requirements = list(filter(lambda x: not 'cherrynas' in x, requirements))
+  requirements = subprocess.run(['pip', 'freeze'],\
+    capture_output=True,\
+    text=True).stdout.splitlines()
+  del_self_package = list(filter(lambda x: not 'cherrynas' in x, requirements))
   
   with open('requirements.txt', 'w') as f:
-    f.write("\r\n".join(requirements))
+    f.write("\r\n".join(del_self_package))
 
   with open('requirements.py', 'w') as f:
     f.write("req = [\r\n")
-    for r in requirements:
-      if 'cherrynas' in r: continue
-      f.write("  '" + r + "',\r\n")
+    for req in del_self_package:
+      if 'cherrynas' in req:
+        continue
+      f.write(f"  '{req}',\r\n")
     f.write("]\r\n")
   print("done")
