7e2a5cac4b8830db8dfda6274285cb9d9c07bb35
JungJik Lee <fnwinter@gmail.com>
2022-04-30 00:13:16 +0900

    version_up

--- cherrynas/scripts/version_up.sh
+++ cherrynas/scripts/version_up.sh
@@ -27,7 +27,9 @@ if [[ "$GIT_TITLE" == *"$SUB"* ]]; then
   python3 -c 'from config.version import save_version_patch
 save_version_patch()'
   NEW_VERSION=`cat ./config/version.txt`
+  git config --global user.email "fnwinter@gmail.com"
+  git config --global user.name "JungJik Lee"
   git add --all
-  git commit -m "version : " + $NEW_VERSION
+  git commit -m "version : ${NEW_VERSION}"
   git push origin main
 fi
