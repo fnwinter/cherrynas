b35005f4def76392048aa5643160e958ef58042c
JungJik Lee <fnwinter@gmail.com>
2023-06-11 09:14:01 +0900

    fix pylint

--- cherrynas/cherrynas/cherrynas.py
+++ cherrynas/cherrynas/cherrynas.py
@@ -1,14 +1,14 @@
 """Welcome to Pynecone! This file outlines the steps to create a basic app."""
-from pcconfig import config
-
 import pynecone as pc
+
+from pcconfig import config
 
 docs_url = "https://pynecone.io/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
 
 class State(pc.State):
     """The app state."""
-    pass
+    print("pass")
 
 def index():
     return pc.text("hello world")
